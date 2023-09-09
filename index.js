95040fd21c509337ce8e2f956496b21c6c9334a4
Manuel <damanuel2007@gmail.com>
2023-09-09 15:55:54 -0300

    added functionality to the backend

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ app.use(cors())
 app.use(express.static('build'))
 const Note = require('./models/note')
 
-
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if(error.name === 'CastError'){
+        return response.status(400).send({error: 'malformatted id'})
+    }
+    next(error)
+}
 
 let notes = [
   {
@@ -38,18 +44,38 @@ app.get('/api/notes', (request, response) => {
     
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-      response.json(note)
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
   })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updateNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -69,7 +95,7 @@ app.post('/api/notes', (request, response) => {
     })
     
 })
-
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
--- mongo.js
+++ mongo.js
@@ -7,8 +7,7 @@ if (process.argv.length<3) {
 
 const password = process.argv[2]
 
-const url =
-`mongodb+srv://fullstack:${password}@cluster0.i8ugd2l.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
