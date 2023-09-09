325a8697241883f56e472a9b6a139211082eee47
Manuel <damanuel2007@gmail.com>
2023-09-09 14:47:07 -0300

    uibuild

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
+const Note = require('./models/note')
+
+
+
 let notes = [
   {
     id: 1,
@@ -27,18 +32,17 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
+    
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if(note){
-    response.json(note)
-    } else {
-        response.status(404).end()
-    }
-})
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
+  })
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -46,34 +50,27 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-    return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
     
-    if(!body.content){
+    if(body.content===undefined){
         return response.status(400).json({
             error: 'content missing'
         })
     }
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId()
-    }
+    })
+       
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
     
-    
-    
-    notes = notes.concat(note)
-    response.json(note)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
